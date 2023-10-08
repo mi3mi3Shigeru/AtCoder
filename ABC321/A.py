88fe08d02adbb27530e099824f1a5fe38bea59e5
mi3mi3Shigeru <minamoto1121@icloud.com>
2023-10-08 18:33:14 +0900

    Create A.py

---
+++ ABC321/A.py
@@ -0,0 +1,12 @@
+N=input()
+digit_count = len(str(N))
+is_like_Number=True
+
+for i in range(digit_count-1):
+    if int(N[i])<=int(N[i+1]):
+        is_like_Number=False
+        break
+if is_like_Number:
+    print("Yes")
+else:
+    print("No")

97ff4dc416b1020cc4ab287744a433ee7eb4162c
mi3mi3Shigeru <minamoto1121@icloud.com>
2023-10-08 19:56:07 +0900

    Update test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+n,m= map(int, input().split())
+s=input()
+t=input()
+is_prefix=False
+is_suffix=False
+answer=3
+
+if str(s) in str(t[0:n]):
+    is_prefix=True
+
+if str(s) in str(t[m-n:m]):
+    is_suffix=True
+
+
+
+    
+if is_prefix:
+    if is_suffix:
+        answer=0
+    else:
+        answer=1
+else:
+    if is_suffix:
+        answer=2
+    else:
+        answer=3
+
+print(str(answer))

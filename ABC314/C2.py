c17220b4e7afd2fbbfb4c8ca499ea217fdbf175d
mi3mi3Shigeru <minamoto1121@icloud.com>
2023-10-18 19:03:03 +0900

    Create C2.py

---
+++ ABC314/C2.py
@@ -0,0 +1,20 @@
+n,m=map(int, input().split())
+S=input()
+c=list(map(int, input().split()))
+
+s=[char for char in S]
+
+a=[]
+a=[[] for _ in range(m)]
+for i in range(n):
+    a[c[i]-1].append(i)
+
+print(a)
+ans=s.copy()
+for b in a:
+    for i in range(len(b)-1,0,-1):
+        ans[b[i]]=s[b[i-1]]
+    ans[b[0]]=s[b[len(b)-1]]
+
+for i in ans:
+    print(i,end="")
